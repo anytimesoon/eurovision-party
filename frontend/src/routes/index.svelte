826993bd41e42311d0ff276285b9ca9c00bd305a
Damien De Lartigue <damien.delartigue@paddypowerbetfair.com>
2022-06-22 18:40:16 +0100

    update to frontend

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import { onMount } from "svelte";
-import { loop_guard } from "svelte/internal";
+    import { loop_guard } from "svelte/internal";
 
     type Country = {
         id: string
@@ -21,24 +21,42 @@ import { loop_guard } from "svelte/internal";
     let countries: Country[];
     let promise = getCountries();
 
-    onMount(async function() {
-        promise = getCountries();
-    });
+    onMount( () => promise = getCountries() );
 
-    
-    
+    function updateParticipating(countryName: string, participatingStatus: boolean) {
+        fetch("http://localhost:8080/country",{
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "name": countryName,
+                "participating": participatingStatus
+            })
+        })
+    }  
 </script>
 
 <h1>List of all Eurovision countries</h1>
 
-<ol>
+<ul style="text-decoration: none;">
     {#await promise} 
         <p>hello</p>
     {:then countries}
         {#each countries as country}
-            <li>{country.flag} {country.name} {#if country.participating } is in the running 🎉 {:else} is out of the running 😢 {/if}</li>
+            <li>
+                {country.flag} 
+                {country.name} 
+                {#if country.participating } 
+                    is in the running 🎉
+                {:else} 
+                    is out of the running 😢
+                {/if}
+                <button on:click="{() => updateParticipating(country.name, country.participating)}">toggle</button>
+            </li>
         {/each}
     {:catch error}
         <p>{error.message}</p>
     {/await}
-</ol>
+</ul>
