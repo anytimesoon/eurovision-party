6e89f4dd4cf41bd8dd1ac5ccfe4c5e56be06649b
Damien De Lartigue <damien.delartigue@paddypowerbetfair.com>
2022-07-03 00:12:48 +0100

    add attributes to votes

--- pkg/domain/vote.go
+++ pkg/domain/vote.go
@@ -3,8 +3,11 @@ package domain
 import "github.com/google/uuid"
 
 type Vote struct {
-	Id        uuid.UUID `json:"id"`
-	UserId    uuid.UUID `json:"userId"`
-	CountryId uuid.UUID `json:"countryId"`
-	Value     int
+	Id          uuid.UUID `json:"id"`
+	UserId      uuid.UUID `json:"userId"`
+	CountryId   uuid.UUID `json:"countryId"`
+	Costume     int       `json:"costume"`
+	Song        int       `json:"song"`
+	Performance int       `json:"performance"`
+	Props       int       `json:"props"`
 }
