6fc60d112934504a1bcdb16989e1e898a6b5e52c
Damien De Lartigue <damien.delartigue@paddypowerbetfair.com>
2022-07-11 11:11:49 +0100

    improve slugify method

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -4,6 +4,9 @@ import "strings"
 
 func Slugify(name string) string {
 	splitName := strings.Split(name, " ")
+	for i, name := range splitName {
+		splitName[i] = strings.ToLower(name)
+	}
 	slug := strings.Join(splitName, "-")
 	return slug
 }
