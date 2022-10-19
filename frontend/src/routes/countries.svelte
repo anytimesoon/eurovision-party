88f97f6dd700af527ec39b6a01d08d930ea118e5
Damien De Lartigue <damien.delartigue@paddypowerbetfair.com>
2022-10-19 09:55:50 +0100

    adding notes

---
+++ frontend/src/routes/countries.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { loop_guard } from "svelte/internal";
+
+    type Country = {
+        id: string
+        name: string
+        bandName: string
+        songName: string
+        flag: string
+        participating: boolean
+    }
+
+    async function getCountries(){
+        const response = await fetch('http://localhost:8080/');
+        countries = await response.json();
+
+        return countries
+    }
+
+    let countries: Country[];
+    let promise = getCountries();
+
+    onMount( () => promise = getCountries() );
+
+    function updateParticipating(country: Country) {
+        fetch('http://localhost:8080/country',{
+            method: "PUT",
+            mode: 'cors',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(country)
+        }).then(response => response.json())
+        .then(data => {
+            countries.find((country) => {if(data.id == country.id){country = data}})
+        })
+        .catch((err) => {
+           console.log(err)
+        })
+    };
+</script>
+
+<h1>List of all Eurovision countries</h1>
+
+<ul style="text-decoration: none;">
+    {#await promise} 
+        <p>loading</p>
+    {:then countries}
+        {#each countries as country}
+            <li>
+                {country.flag} 
+                {country.name} 
+                {#if country.participating } 
+                    is in the running 🎉
+                {:else} 
+                    is out of the running 😢
+                {/if}
+                <button on:click="{() => updateParticipating(country)}">toggle</button>
+            </li>
+        {/each}
+    {:catch error}
+        <p>{error.message}</p>
+    {/await}
+</ul>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,66 +1,17 @@
-<script lang="ts">
-    import { onMount } from "svelte";
-    import { loop_guard } from "svelte/internal";
+<script>
+	let user = { loggedIn: false };
 
-    type Country = {
-        id: string
-        name: string
-        bandName: string
-        songName: string
-        flag: string
-        participating: boolean
-    }
-
-    async function getCountries(){
-        const response = await fetch('http://localhost:8080/');
-        countries = await response.json();
-
-        return countries
-    }
-
-    let countries: Country[];
-    let promise = getCountries();
-
-    onMount( () => promise = getCountries() );
-
-    function updateParticipating(country: Country) {
-        fetch('http://localhost:8080/country',{
-            method: "PUT",
-            mode: 'cors',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(country)
-        }).then(response => response.json())
-        .then(data => {
-            countries.find((country) => {if(data.id == country.id){country = data}})
-        })
-        .catch((err) => {
-           console.log(err)
-        })
-    };
+	function toggle() {
+		user.loggedIn = !user.loggedIn;
+	}
 </script>
 
-<h1>List of all Eurovision countries</h1>
-
-<ul style="text-decoration: none;">
-    {#await promise} 
-        <p>hello</p>
-    {:then countries}
-        {#each countries as country}
-            <li>
-                {country.flag} 
-                {country.name} 
-                {#if country.participating } 
-                    is in the running 🎉
-                {:else} 
-                    is out of the running 😢
-                {/if}
-                <button on:click="{() => updateParticipating(country)}">toggle</button>
-            </li>
-        {/each}
-    {:catch error}
-        <p>{error.message}</p>
-    {/await}
-</ul>
+{#if user.loggedIn}
+	<button on:click={toggle}>
+		Log out
+	</button>
+{:else}
+	<button on:click={toggle}>
+		Log in
+	</button>
+{/if}
