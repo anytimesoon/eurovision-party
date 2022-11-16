c14a5f63f916af7bf8109ec4f8c943b316e2af21
Damien de Lartigue <damien@delartigue.co.uk>
2022-11-16 19:31:31 +0100

    add site layout

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,27 @@
+<script>
+    import "../app.css";
+  </script>
+  
+  <svelte:head>
+    <meta name="title" content="Eurovision party" />
+    <meta name="description" content="Chat and vote in a private remote party" />
+  </svelte:head>
+  
+  <nav>
+    <a href=".">HOME</a>
+    <a href="/about">ABOUT</a>
+  </nav>
+  
+  <slot />
+  
+  <style>
+    nav {
+      padding: 1rem;
+    }
+    a {
+      text-decoration: none;
+      color: gray;
+      margin-right: 1rem;
+    }
+  </style>
+  
