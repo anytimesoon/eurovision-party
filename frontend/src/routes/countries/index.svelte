21f6e093bc91bb3067107f0d919d8c861a088a8a
Damien De Lartigue <damien.delartigue@paddypowerbetfair.com>
2022-12-23 17:24:00 +0000

    create working country store

--- frontend/src/routes/countries/index.svelte
+++ frontend/src/routes/countries/index.svelte
@@ -1,47 +1,36 @@
-<script lang="ts" context="module">
-    import {getCountries} from "$lib/stores/country.store";
-    import {updateCountry} from "$lib/stores/country.store";
-    getCountries();
-</script>
-
 <script lang="ts">
-    import type {CountryModel} from "$lib/models/classes/country.model"
+    import type {CountryModel} from "$lib/models/classes/country.model";
 	import { countryStore } from "$lib/stores/country.store";
+    import { countryEP } from "$lib/models/enums/endpoints.enum"
+	import { onMount } from "svelte";
+    import { sendGet } from '$lib/modules/sender';
 
-    let countries:CountryModel[];
-    countryStore.subscribe(val => {
-            countries = val;
-        });
+    onMount( () => {
+        sendGet<CountryModel[]>(countryEP.ALL).then( data => $countryStore = data.body);
+    })
 </script>
 
 <div style="width: 50%">
     <h1>List of all Eurovision countries</h1>
 
     <ul id="full-list" style="text-decoration: none;">
-        {#await countries} 
-            <p>loading</p>
-        {:then countries}
-            {#if countries.length > 0}
-                {#each countries as country}
-                    <li>
-                        {country.flag} 
-                        {country.name} 
-                        {#if country.participating } 
-                            is in the running 🎉
-                        {:else} 
-                            is out of the running 😢
-                        {/if}
-                        <button on:click="{() => updateCountry(country)}">toggle</button>
-                    </li>
-                {/each}
-            {/if}
-        {:catch error}
-            <p>{error.message}</p>
-        {/await}
-    </ul>
-</div>
 
-<div style="width: 50%">
+        {#if $countryStore.length > 0}
+        {#each $countryStore as country}
+        <li>
+            {country.flag}
+            {country.name}
+            {#if country.participating }
+            is in the running 🎉
+            {:else}
+            is out of the running 😢
+            {/if}
+<!--            <button on:click="{() => sendPost()}">toggle</button>-->
+        </li>
+        {/each}
+        {/if}
+    </ul>
+
     <ul id="active-list" style="text-decoration: none;">
 
     </ul>
