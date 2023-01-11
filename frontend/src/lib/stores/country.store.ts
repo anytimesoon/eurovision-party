e67afb37742a461083fdbd6c87639a3e64c15dbb
anytimesoon <damien@delartigue.co.uk>
2023-01-11 11:15:08 +0100

    add readable country story

--- frontend/src/lib/stores/country.store.ts
+++ frontend/src/lib/stores/country.store.ts
@@ -1,4 +1,12 @@
-import { writable } from "svelte/store";
+import {readable, writable} from "svelte/store";
 import type { CountryModel } from "$lib/models/classes/country.model";
-  
+import {browser} from "$app/env";
+
+const defaultCountryList:CountryModel[] = [];
 export const countryStore = writable<CountryModel[]>([]);
+
+export const partCountryStore = readable<CountryModel[]>(browser && JSON.parse(localStorage.getItem("partCountryStore") || JSON.stringify(defaultCountryList)));
+
+partCountryStore.subscribe((val) => {
+    browser && localStorage.setItem("partCountryStore", JSON.stringify(val))
+});
