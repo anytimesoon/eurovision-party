b7bfa18b2131d8d700e68e68d6a45d2d79e4a2da
anytimesoon <damien@delartigue.co.uk>
2023-01-11 11:13:24 +0100

    remove unused functions from country dto

--- pkg/dto/countryDTO.go
+++ pkg/dto/countryDTO.go
@@ -1,11 +1,7 @@
 package dto
 
 import (
-	"eurovision/pkg/errs"
-	"fmt"
-
 	"github.com/google/uuid"
-	"github.com/jmoiron/sqlx"
 )
 
 type Country struct {
@@ -16,41 +12,4 @@ type Country struct {
 	SongName      string    `json:"songName"`
 	Flag          string    `json:"flag"`
 	Participating bool      `json:"participating"`
-}
-
-func (c Country) Validate() *errs.AppError {
-	messages := make([]string, 0)
-
-	message := isPresent(c.BandName, "Band name")
-	if message != "" {
-		messages = append(messages, message)
-	}
-
-	message = isPresent(c.SongName, "Song name")
-	if message != "" {
-		messages = append(messages, message)
-	}
-
-	return messagesToError(messages)
-}
-
-func (c Country) GetName() string {
-	return c.Name
-}
-
-func (c *Country) SetSlug(slug string) {
-	c.Slug = slug
-}
-
-func (c Country) VerifySlug(slug string, db *sqlx.DB) bool {
-	query := fmt.Sprintf("SELECT * FROM country WHERE slug = '%s'", slug)
-	row, err := db.Queryx(query)
-	if err != nil {
-		return false
-	}
-
-	if row.Rows != nil {
-		return false
-	}
-	return true
 }
