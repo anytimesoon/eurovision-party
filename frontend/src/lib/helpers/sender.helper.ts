bb63c2d1f2be4a3478741a3482cabea8a4f99967
anytimesoon <damien@delartigue.co.uk>
2023-01-11 11:03:15 +0100

    update sender to include both put or post methods

--- frontend/src/lib/helpers/sender.helper.ts
+++ frontend/src/lib/helpers/sender.helper.ts
@@ -1,4 +1,3 @@
-import { RequestModel } from "$lib/models/classes/request.model";
 import { ResponseModel } from "$lib/models/classes/response.model"
 import { TokenModel } from "$lib/models/classes/token.model";
 import { tokenStore } from '$lib/stores/token.store';
@@ -34,18 +33,19 @@ export async function sendGet<T>(endpoint : string): Promise<ResponseModel<T>> {
     return resp
 }
 
-export async function sendPost<T, U>(endpoint : string, payload : T): Promise<ResponseModel<U>> {
+export async function sendCreateOrUpdate<T, U>(endpoint: string, payload: T, method = "PUT"): Promise<ResponseModel<U>> {
     let resp = new ResponseModel<U>();
     let t = new TokenModel();
     tokenStore.subscribe(
         val => t = val
     )
 
-    const request = new RequestModel<T>().build(t.token, payload);
-
     await fetch(endpoint, {
-        method: "POST",
-        body: JSON.stringify(request)
+        method: method,
+        headers: {
+            "Authorization": t.token
+        },
+        body: JSON.stringify(payload)
     }).
     then(
         res => res.json() as Promise<ResponseModel<U>>
