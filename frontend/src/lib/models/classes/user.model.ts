c40383743f89e177e1ab441b9240176d3e9c7298
anytimesoon <damien@delartigue.co.uk>
2023-01-15 23:44:31 +0100

    add authLvl to user model

--- frontend/src/lib/models/classes/user.model.ts
+++ frontend/src/lib/models/classes/user.model.ts
@@ -3,10 +3,11 @@ import type { IDeserializable } from '../interfaces/ideserializable.interface';
 
 export class UserModel implements IDeserializable<IUser>, IUser {
 
-    public id!:   string;
-	public name!: string;
-	public slug!: string;
-	public icon!: string;
+    public id!:      string;
+	public name!:    string;
+	public slug!:    string;
+	public icon!:    string;
+	public authLvl!: number;
 
 	deserialize(input: IUser): this {
 		Object.assign(this, input);
