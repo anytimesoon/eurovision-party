121ee0e2621fa498951002ae6fa0af11ba499569
anytimesoon <damien@delartigue.co.uk>
2023-04-10 15:23:30 +0200

    fix websocket not loading on login

---
+++ frontend/src/routes/__layout-login.svelte
@@ -0,0 +1,12 @@
+<script>
+    import "../app.css";
+</script>
+
+<svelte:head>
+    <meta name="title" content="Eurovision party" />
+    <meta name="description" content="Chat and vote in a private remote party" />
+</svelte:head>
+
+<main>
+    <slot />
+</main>
