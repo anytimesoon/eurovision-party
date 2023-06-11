fb093de4059752533fc8d905bc3a85ef60447897
Damien de Lartigue <damien@delartigue.co.uk>
2023-06-11 18:52:45 +0100

    fix forgotten import after refactor

--- frontend/src/lib/stores/user.store.ts
+++ frontend/src/lib/stores/user.store.ts
@@ -1,5 +1,6 @@
 import {writable} from "svelte/store";
 import {UserModel} from "$lib/models/classes/user.model";
+import {browser} from "$app/environment";
 
 export const currentUser = writable<UserModel>(browser && JSON.parse(localStorage.getItem("currentUser") || JSON.stringify(new UserModel())))
 
