1fa2968e00ab63e02461310e2a5135f72e0364f9
anytimesoon <damien@delartigue.co.uk>
2023-06-22 23:47:55 +0200

    add user name updates to frontend

--- frontend/src/routes/(app)/settings/+page.server.ts
+++ frontend/src/routes/(app)/settings/+page.server.ts
@@ -1,24 +1,58 @@
 import type {Actions} from "./$types";
-import {voteGoEP} from "$lib/models/enums/endpoints.enum";
+import {userGoEP, voteGoEP} from "$lib/models/enums/endpoints.enum";
 import {ResponseModel} from "$lib/models/classes/response.model";
 import {ResultModel} from "$lib/models/classes/result.model";
+import type {UserModel} from "$lib/models/classes/user.model";
 
 export const actions : Actions = {
     showNameForm: async ({fetch, request}) => {
-        // const fd = await request.formData()
 
         return {
-            hideNameForm: false
+            hideNameForm: false,
+            hideImgForm: true
         }
     },
     updateName: async ({fetch, request}) => {
         const fd = await request.formData()
-        const user = Object.fromEntries(fd)
+        const user:UserModel = Object.fromEntries([...fd]) as UserModel
 
-        console.log(user)
+        const res = await fetch(userGoEP.UPDATE, {
+            method: "PUT",
+            body: JSON.stringify(user)
+        })
+
+        const userResp:ResponseModel<UserModel> = await res.json()
 
         return {
-            hideNameForm: true
+            hideNameForm: true,
+            hideImgForm: true,
+            error: userResp.error,
+            user: user
+        }
+    },
+    showImgForm: async ({fetch, request}) => {
+
+        return {
+            hideNameForm: true,
+            hideImgForm: false
+        }
+    },
+    updateImg: async ({fetch, request}) => {
+        const fd = await request.formData()
+        const user:UserModel = Object.fromEntries([...fd]) as UserModel
+
+        const res = await fetch(userGoEP.UPDATE, {
+            method: "PUT",
+            body: JSON.stringify(user)
+        })
+
+        const userResp:ResponseModel<UserModel> = await res.json()
+
+        return {
+            hideNameForm: true,
+            hideImgForm: true,
+            error: userResp.error,
+            user: user
         }
     }
 }
--- frontend/src/routes/(app)/settings/+page.svelte
+++ frontend/src/routes/(app)/settings/+page.svelte
@@ -3,13 +3,21 @@
     import {staticEP} from "$lib/models/enums/endpoints.enum";
     import { enhance } from '$app/forms';
     import type {ActionData} from './$types';
+    import Cropper from 'svelte-easy-crop';
+
+    let image = staticEP.IMG + $currentUser.icon
+    let crop = { x: 0, y: 0 }
+    let zoom = 1
+    let aspect = 1
 
     export let form:ActionData
     let hideNameForm = true
+    let hideImgForm = true
 
     function updateform(form: ActionData) {
         if (form != null) {
             hideNameForm = form.hideNameForm
+            hideImgForm = form.hideImgForm
         }
     }
 
@@ -26,7 +34,24 @@
 {:else}
     <form method="POST" action="?/updateName" use:enhance>
         <input type="text" name="name" bind:value={$currentUser.name} />
+        <input type="hidden" name="id" bind:value={$currentUser.id} />
+        <input type="hidden" name="email" bind:value={$currentUser.email} />
         <button>Save</button>
     </form>
 {/if}
-<img src={staticEP.IMG + $currentUser.icon} alt={$currentUser.name + "'s avatar"} style="max-width: 100px">
+
+{#if hideImgForm }
+    <img src={staticEP.IMG + $currentUser.icon} alt={$currentUser.name + "'s avatar"} style="max-width: 100px">
+    <form method="POST" action="?/showImgForm" use:enhance>
+        <button>Edit</button>
+    </form>
+{:else}
+    <Cropper
+            {image}
+            bind:crop
+            bind:zoom
+            bind:aspect
+            on:cropcomplete={e => console.log(e.detail)}
+    />
+    <button type="button" on:click={async () => {image = await getCroppedImg(image, pixelCrop)}}>Crop!</button>
+{/if}
