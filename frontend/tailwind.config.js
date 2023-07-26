38ef836dba5737a9f8137370badc54f887f9d32c
Damien de Lartigue <damien@delartigue.co.uk>
2023-07-26 17:11:31 +0100

    add tailwind elements

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}',
+    "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class"
 }
 
