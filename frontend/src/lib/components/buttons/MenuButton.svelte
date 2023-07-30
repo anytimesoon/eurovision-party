41c3e221715bbee67d673845685b5ac4bb1177e3
Damien de Lartigue <damien@delartigue.co.uk>
2023-07-30 16:41:26 +0100

    fix menu button padding

--- frontend/src/lib/components/buttons/MenuButton.svelte
+++ frontend/src/lib/components/buttons/MenuButton.svelte
@@ -16,10 +16,9 @@
     });
 
     export let icon = ""
-    export let padding = 5;
 </script>
 
-<div class="px-{padding} text-center">
+<div class="px-5 text-center">
 
         {#if icon === "chat" }
             <a class="text-neutral-500 transition duration-200 hover:text-neutral-700 hover:ease-in-out focus:text-neutral-700 disabled:text-black/30 motion-reduce:transition-none dark:text-neutral-200 dark:hover:text-neutral-300 dark:focus:text-neutral-300 [&.active]:text-black/90 dark:[&.active]:text-neutral-400" href="/">
