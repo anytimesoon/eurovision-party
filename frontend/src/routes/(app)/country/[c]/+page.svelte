64aaed8c22fd8530a9ee332b1e0fa2371aab9faf
Damien de Lartigue <damien@delartigue.co.uk>
2023-08-28 14:49:18 +0100

    style voting page

--- frontend/src/routes/(app)/country/[c]/+page.svelte
+++ frontend/src/routes/(app)/country/[c]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import {VoteModel} from "$lib/models/classes/vote.model";
+    import type {VoteModel} from "$lib/models/classes/vote.model";
     import type {CountryModel} from "$lib/models/classes/country.model";
     import type {ActionData, PageData} from './$types';
     import {currentUser} from "$lib/stores/user.store";
@@ -23,62 +23,99 @@
     $: updateVote(form)
 </script>
 
+<div class="h-full overflow-auto p-3">
+    <h1 class="text-center">{country.flag} {country.name}</h1>
 
-<h1>{country.flag} {country.name}</h1>
+    {#if country.songName || country.bandName}
+        <h3>{country.songName}</h3>
+        <h5>{country.bandName}</h5>
+    {/if}
 
-{#if country.songName || country.bandName}
-    <h3>{country.songName}</h3>
-    <h5>{country.bandName}</h5>
-{/if}
+    <div class="py-5">
+        <h4 class="text-center">🎵 Song {vote.song}/10</h4>
+        <form method="POST" action="?/vote" use:enhance>
+            <input name="countrySlug" type="hidden" value={country.slug}>
+            <input name="cat" type="hidden" value="song">
+            <input name="userId" type="hidden" value={$currentUser.id}>
+            <div class="flex flex-row-reverse items-center mx-auto justify-between">
+                {#each voteOptions.reverse() as { key, label }}
+                    <input id="song-star-{label}" class="hidden peer" type="radio" bind:group={vote.song} value={key} name="score" on:click={(e) => {
+                        e.target.parentElement.requestSubmit()
+                    }}/>
+                    <label
+                            class="text-yellow-400 ease-in-out duration-300 text-4xl
+                            hover:text-yellow-500
+                            before:content-['\2606']
+                            peer-checked:before:content-['\2605']"
+                            for="song-star-{label}"></label>
+                {/each}
+            </div>
+        </form>
+    </div>
 
-<h4>Song</h4>
-<form method="POST" action="?/vote" use:enhance>
-    <input name="countrySlug" type="hidden" value={country.slug}>
-    <input name="cat" type="hidden" value="song">
-    <input name="userId" type="hidden" value={$currentUser.id}>
-    {#each voteOptions as { key, label }}
-        <input type="radio" bind:group={vote.song} value={key} name="score" on:click={(e) => {
-            e.target.parentElement.requestSubmit()
-        }}/>
-        <label>{label}</label>
-    {/each}
-</form>
+    <div class="py-5">
+        <h4 class="text-center">💃 Performance {vote.performance}/10</h4>
+        <form method="POST" action="?/vote" use:enhance>
+            <input name="countrySlug" type="hidden" value={country.slug}>
+            <input name="cat" type="hidden" value="performance">
+            <input name="userId" type="hidden" value={$currentUser.id}>
+            <div class="flex flex-row-reverse items-center mx-auto justify-between">
+                {#each voteOptions as { key, label }}
+                    <input id="performance-star-{label}" class="hidden peer" type="radio" bind:group={vote.performance} value={key} name="score" on:click={(e) => {
+                        e.target.parentElement.requestSubmit()
+                    }}/>
+                    <label
+                            class="text-yellow-400 ease-in-out duration-300 text-5xl
+                            hover:text-yellow-500
+                            before:content-['\2606']
+                            peer-checked:before:content-['\2605']"
+                            for="performance-star-{label}"></label>
+                {/each}
+            </div>
+        </form>
+    </div>
 
-<h4>Performance</h4>
-<form method="POST" action="?/vote" use:enhance>
-    <input name="countrySlug" type="hidden" value={country.slug}>
-    <input name="cat" type="hidden" value="performance">
-    <input name="userId" type="hidden" value={$currentUser.id}>
-    {#each voteOptions as { key, label }}
-        <input type="radio" bind:group={vote.performance} value={key} name="score" on:click={(e) => {
-            e.target.parentElement.requestSubmit()
-        }}/>
-        <label>{label}</label>
-    {/each}
-</form>
+    <div class="py-5">
+        <h4 class="text-center">🥻 Costume {vote.costume}/10</h4>
+        <form method="POST" action="?/vote" use:enhance>
+            <input name="countrySlug" type="hidden" value={country.slug}>
+            <input name="cat" type="hidden" value="costume">
+            <input name="userId" type="hidden" value={$currentUser.id}>
+            <div class="flex flex-row-reverse items-center mx-auto justify-between">
+                {#each voteOptions as { key, label }}
+                    <input id="costume-star-{label}" class="hidden peer" type="radio" bind:group={vote.costume} value={key} name="score" on:click={(e) => {
+                        e.target.parentElement.requestSubmit()
+                    }}/>
+                    <label
+                            class="text-yellow-400 ease-in-out duration-300 text-5xl
+                            hover:text-yellow-500
+                            before:content-['\2606']
+                            peer-checked:before:content-['\2605']"
+                            for="costume-star-{label}"></label>
+                {/each}
+            </div>
+        </form>
+    </div>
 
-<h4>Costume</h4>
-<form method="POST" action="?/vote" use:enhance>
-    <input name="countrySlug" type="hidden" value={country.slug}>
-    <input name="cat" type="hidden" value="costume">
-    <input name="userId" type="hidden" value={$currentUser.id}>
-    {#each voteOptions as { key, label }}
-        <input type="radio" bind:group={vote.costume} value={key} name="score" on:click={(e) => {
-            e.target.parentElement.requestSubmit()
-        }}/>
-        <label>{label}</label>
-    {/each}
-</form>
-
-<h4>Staging and Props</h4>
-<form method="POST" action="?/vote" use:enhance>
-    <input name="countrySlug" type="hidden" value={country.slug}>
-    <input name="cat" type="hidden" value="props">
-    <input name="userId" type="hidden" value={$currentUser.id}>
-    {#each voteOptions as { key, label }}
-        <input type="radio" bind:group={vote.props} value={key} name="score" on:click={(e) => {
-            e.target.parentElement.requestSubmit()
-        }}/>
-        <label>{label}</label>
-    {/each}
-</form>
+    <div class="py-5">
+        <h4 class="text-center">🎆 Staging and Props {vote.props}/10</h4>
+        <form method="POST" action="?/vote" use:enhance>
+            <input name="countrySlug" type="hidden" value={country.slug}>
+            <input name="cat" type="hidden" value="props">
+            <input name="userId" type="hidden" value={$currentUser.id}>
+            <div class="flex flex-row-reverse items-center mx-auto justify-between">
+                {#each voteOptions as { key, label }}
+                    <input id="props-star-{label}" class="hidden peer" type="radio" bind:group={vote.props} value={key} name="score" on:click={(e) => {
+                        e.target.parentElement.requestSubmit()
+                    }}/>
+                    <label
+                            class="text-yellow-400 ease-in-out duration-300 text-5xl
+                            hover:text-yellow-500
+                            before:content-['\2606']
+                            peer-checked:before:content-['\2605']"
+                            for="props-star-{label}"></label>
+                {/each}
+            </div>
+        </form>
+    </div>
+</div>
