3e6651795561e08710e45133ce26e2ad58212460
Damien de Lartigue <damien@delartigue.co.uk>
2023-08-29 18:18:19 +0100

    add page to enter act details

--- frontend/src/lib/components/AdminNav.svelte
+++ frontend/src/lib/components/AdminNav.svelte
@@ -1,5 +1,6 @@
 <ul class="mx-auto max-w-max p-3">
     <li class="inline m-1.5"><a href="/settings">Personal</a></li>
     <li class="inline m-1.5"><a href="/admin/countries">Countries</a></li>
+    <li class="inline m-1.5"><a href="/admin/acts">Acts</a></li>
     <li class="inline m-1.5"><a href="/admin/friends">Friends</a></li>
 </ul>
---
+++ frontend/src/routes/(app)/admin/acts/+page.server.ts
@@ -0,0 +1,28 @@
+import {ResponseModel} from "$lib/models/classes/response.model";
+import {CountryModel} from "$lib/models/classes/country.model";
+import type {Actions} from "./$types";
+import {countryGoEP} from "$lib/models/enums/endpoints.enum";
+
+export const actions : Actions = {
+    update: async ({fetch, request}) => {
+        const fd = await request.formData()
+        const country:CountryModel = Object.fromEntries([...fd]) as CountryModel;
+        const resProm = await fetch(countryGoEP.UPDATE, {
+            method: "PUT",
+            body: JSON.stringify(country, (key, value) => {
+                if (key === "participating") {
+                    return value === "on"
+                } else {
+                    return value
+                }})
+        })
+
+        const res:ResponseModel<CountryModel> = await resProm.json()
+
+        return {
+            success: true,
+            country: res.body,
+            error: res.error
+        }
+    }
+}
