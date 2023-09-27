07be2771220bb2b918f9614fd83b139bad7c10d0
Damien de Lartigue <damien@delartigue.co.uk>
2023-09-27 18:25:26 +0100

    update spinner

--- frontend/src/lib/components/Spinner.svelte
+++ frontend/src/lib/components/Spinner.svelte
@@ -1,12 +1,11 @@
-<div class="fixed
-            z-50
-            left-0
-            right-0
-            top-0
-            bottom-0
-            bg-no-repeat
-            bg-center
-            transition-all
-            ease-in-out
-            duration-300
-            bg-[url('data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='40' height='40' viewBox='0 0 50 50'%3E%3Cpath d='M28.43 6.378C18.27 4.586 8.58 11.37 6.788 21.533c-1.791 10.161 4.994 19.851 15.155 21.643l.707-4.006C14.7 37.768 9.392 30.189 10.794 22.24c1.401-7.95 8.981-13.258 16.93-11.856l.707-4.006z'%3E%3CanimateTransform attributeType='xml' attributeName='transform' type='rotate' from='0 25 25' to='360 25 25' dur='0.6s' repeatCount='indefinite'/%3E%3C/path%3E%3C/svg%3E')]"></div>
+<div class="flex h-full w-full items-center justify-center">
+    <div class="w-12
+            h-12
+            rounded-full
+            animate-spin
+            border-8
+            border-solid
+            border-purple-500
+            border-t-transparent"></div>
+</div>
+
