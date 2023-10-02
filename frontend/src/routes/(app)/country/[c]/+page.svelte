4d8dced31dce17885641a9da24a06a42858d208b
anytimesoon <damien@delartigue.co.uk>
2023-10-02 18:13:51 +0200

    update and simplify voting page by using new vote form component

--- frontend/src/routes/(app)/country/[c]/+page.svelte
+++ frontend/src/routes/(app)/country/[c]/+page.svelte
@@ -2,29 +2,19 @@
     import type {VoteModel} from "$lib/models/classes/vote.model";
     import type {CountryModel} from "$lib/models/classes/country.model";
     import type {ActionData, PageData} from './$types';
-    import {currentUser} from "$lib/stores/user.store";
-    import {voteOptions} from "$lib/models/classes/voteOptions.model";
-    import { enhance } from '$app/forms';
+    import VoteForm from "$lib/components/forms/VoteForm.svelte";
 
     export let data:PageData
     export let form:ActionData
     let country:CountryModel = data.country
     let vote:VoteModel = data.vote
-    let songForm:HTMLFormElement
-    let perfForm:HTMLFormElement
-    let costForm:HTMLFormElement
-    let propForm:HTMLFormElement
-
-    const updateVote = (form) => {
-        if(form != null) {
-            vote = form.vote
-        }
-    }
 
 
     $: country = data.country
     $: vote = data.vote
-    $: updateVote(form)
+    $: if (form) {
+        vote = form.vote
+    }
 </script>
 
 <div class="h-full overflow-auto p-3">
@@ -32,97 +22,14 @@
 
     <div class="text-center py-3">
         {#if country.songName || country.bandName}
-            <h3>{country.songName}</h3>
-            <p>performed by</p>
-            <h5>{country.bandName}</h5>
+            <h4>{country.songName} <span class="text-xs text-typography-grey">by</span> {country.bandName}</h4>
         {/if}
     </div>
 
-    <div class="py-5">
-        <h4 class="text-center"><i class="fa-solid fa-music"></i> Song {vote.song}/10</h4>
-        <form method="POST" action="?/vote" use:enhance bind:this={songForm}>
-            <input name="countrySlug" type="hidden" value={country.slug}>
-            <input name="cat" type="hidden" value="song">
-            <input name="userId" type="hidden" value={$currentUser.id}>
-            <div class="flex flex-row-reverse items-center mx-auto justify-between">
-                {#each voteOptions.reverse() as { key, label }}
-                    <input id="song-star-{label}" class="hidden peer" type="radio" bind:group={vote.song} value={key} name="score" on:click={() => {
-                        songForm.requestSubmit()
-                    }}/>
-                    <label
-                            class="text-yellow-400 ease-in-out duration-300 text-4xl
-                            hover:text-yellow-500
-                            before:content-['\2606']
-                            peer-checked:before:content-['\2605']"
-                            for="song-star-{label}"></label>
-                {/each}
-            </div>
-        </form>
-    </div>
+    <VoteForm vote={vote} catName="Song" countrySlug={country.slug} />
+    <VoteForm vote={vote} catName="Performance" countrySlug={country.slug} />
+    <VoteForm vote={vote} catName="Costumes" countrySlug={country.slug} />
+    <VoteForm vote={vote} catName="Staging and Props" countrySlug={country.slug} />
 
-    <div class="py-5">
-        <h4 class="text-center"><i class="fa-solid fa-masks-theater"></i> Performance {vote.performance}/10</h4>
-        <form method="POST" action="?/vote" use:enhance bind:this={perfForm}>
-            <input name="countrySlug" type="hidden" value={country.slug}>
-            <input name="cat" type="hidden" value="performance">
-            <input name="userId" type="hidden" value={$currentUser.id}>
-            <div class="flex flex-row-reverse items-center mx-auto justify-between">
-                {#each voteOptions as { key, label }}
-                    <input id="performance-star-{label}" class="hidden peer" type="radio" bind:group={vote.performance} value={key} name="score" on:click={() => {
-                        perfForm.requestSubmit()
-                    }}/>
-                    <label
-                            class="text-yellow-400 ease-in-out duration-300 text-4xl
-                            hover:text-yellow-500
-                            before:content-['\2606']
-                            peer-checked:before:content-['\2605']"
-                            for="performance-star-{label}"></label>
-                {/each}
-            </div>
-        </form>
-    </div>
 
-    <div class="py-5">
-        <h4 class="text-center"><i class="fa-solid fa-user-ninja"></i> Costume {vote.costume}/10</h4>
-        <form method="POST" action="?/vote" use:enhance bind:this={costForm}>
-            <input name="countrySlug" type="hidden" value={country.slug}>
-            <input name="cat" type="hidden" value="costume">
-            <input name="userId" type="hidden" value={$currentUser.id}>
-            <div class="flex flex-row-reverse items-center mx-auto justify-between">
-                {#each voteOptions as { key, label }}
-                    <input id="costume-star-{label}" class="hidden peer" type="radio" bind:group={vote.costume} value={key} name="score" on:click={() => {
-                        costForm.requestSubmit()
-                    }}/>
-                    <label
-                            class="text-yellow-400 ease-in-out duration-300 text-4xl
-                            hover:text-yellow-500
-                            before:content-['\2606']
-                            peer-checked:before:content-['\2605']"
-                            for="costume-star-{label}"></label>
-                {/each}
-            </div>
-        </form>
-    </div>
-
-    <div class="py-5">
-        <h4 class="text-center"><i class="fa-solid fa-burst"></i> Staging and Props {vote.props}/10</h4>
-        <form method="POST" action="?/vote" use:enhance bind:this={propForm}>
-            <input name="countrySlug" type="hidden" value={country.slug}>
-            <input name="cat" type="hidden" value="props">
-            <input name="userId" type="hidden" value={$currentUser.id}>
-            <div class="flex flex-row-reverse items-center mx-auto justify-between">
-                {#each voteOptions as { key, label }}
-                    <input id="props-star-{label}" class="hidden peer" type="radio" bind:group={vote.props} value={key} name="score" on:click={() => {
-                        propForm.requestSubmit()
-                    }}/>
-                    <label
-                            class="text-yellow-400 ease-in-out duration-300 text-4xl
-                            hover:text-yellow-500
-                            before:content-['\2606']
-                            peer-checked:before:content-['\2605']"
-                            for="props-star-{label}"></label>
-                {/each}
-            </div>
-        </form>
-    </div>
 </div>
