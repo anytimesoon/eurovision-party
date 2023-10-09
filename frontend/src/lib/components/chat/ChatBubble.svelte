78a895126da1ad309432db2c20b9c49991b99e99
anytimesoon <damien@delartigue.co.uk>
2023-10-09 18:18:31 +0100

    move user name outside of chat bubble

--- frontend/src/lib/components/chat/ChatBubble.svelte
+++ frontend/src/lib/components/chat/ChatBubble.svelte
@@ -9,6 +9,7 @@
     export let user:UserModel
     export let isCurrentUser:boolean
 
+    $: userNameStyle = isCurrentUser ? "text-right" : ""
     $: currentUserBubbleContainer = isCurrentUser ? "ml-auto justify-end" : ""
     $: currentUserImage = isCurrentUser ? "order-last ml-3" : "mr-3"
     $: currentUserBubble = isCurrentUser ? "bg-chat-bubble-me" : "bg-chat-bubble-you"
@@ -20,13 +21,19 @@
         <p class="text-sm">{comment.text}</p>
     </div>
 {:else}
-    <div class="flex w-full mt-2 max-w-xs {currentUserBubbleContainer}">
 
+
+    <div class="flex w-full mt-2 max-w-xs {currentUserBubbleContainer}">
         <img class="flex-shrink-0 h-10 w-10 rounded-full {currentUserImage}" src={staticEP.IMG + user.icon} alt={user.name + "'s avatar"}>
 
-        <div class="p-3 {roundedCorners} {currentUserBubble}">
-            <ChatContent comment={comment} user={user} isCurrentUser={isCurrentUser}/>
+        <div>
+            <p class="block {userNameStyle}">{user.name}</p>
+            <div class="p-3 {roundedCorners} {currentUserBubble}">
+                <ChatContent comment={comment} user={user} isCurrentUser={isCurrentUser}/>
+            </div>
         </div>
+
+
 
     </div>
 {/if}
--- frontend/src/lib/components/chat/ChatContent.svelte
+++ frontend/src/lib/components/chat/ChatContent.svelte
@@ -6,12 +6,10 @@
     export let user:UserModel
     export let isCurrentUser:boolean
 
-
-    $: userNameStyle = isCurrentUser ? "text-right" : ""
     $: contentTextStyle = isCurrentUser ? "text-typography-chat-me" : "text-typography-chat-you"
     $: timeStyle = isCurrentUser ? "" : "text-right"
 </script>
 
-<p class="block pb-1 {userNameStyle}">{user.name}</p>
+
 <p class="{contentTextStyle} text-sm">{comment.text}</p>
 <span class="text-[0.6rem] block pt-1 text-typography-grey {timeStyle}">{comment.createdAt.getHours() + ":" + (comment.createdAt.getMinutes() < 10 ? "0" + comment.createdAt.getMinutes() : comment.createdAt.getMinutes())}</span>
