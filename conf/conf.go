1b1fa5f47f9878fb74be90622491feaad8c3f6d4
anytimesoon <damien@delartigue.co.uk>
2023-11-13 23:29:31 +0100

    bot id is saved to conf in case of server restart

--- conf/conf.go
+++ conf/conf.go
@@ -4,31 +4,39 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/spf13/viper"
+	"log"
 )
 
 var App AppConf
 
+var v = viper.New()
+
 type AppConf struct {
-	DbUsername string `mapstructure:"DB_USERNAME"`
-	DbPassword string `mapstructure:"DB_PASSWORD"`
-	DbHostname string `mapstructure:"DB_HOSTNAME"`
-	DbPort     string `mapstructure:"DB_PORT"`
-	DbName     string `mapstructure:"DB_NAME"`
-	ServPort   string `mapstructure:"BACKEND_PORT"`
-	ServHost   string `mapstructure:"BACKEND_HOSTNAME"`
-	HttpProto  string `mapstructure:"HTTP_PROTOCOL"`
-	Domain     string `mapstructure:"DOMAIN_NAME"`
-	BotId      uuid.UUID
-	BotName    string `mapstructure:"CHAT_BOT_NAME"`
-	Assets     string `mapstructure:"ASSET_DIR"`
+	DbUsername  string `mapstructure:"DB_USERNAME"`
+	DbPassword  string `mapstructure:"DB_PASSWORD"`
+	DbHostname  string `mapstructure:"DB_HOSTNAME"`
+	DbPort      string `mapstructure:"DB_PORT"`
+	DbName      string `mapstructure:"DB_NAME"`
+	ServPort    string `mapstructure:"BACKEND_PORT"`
+	ServHost    string `mapstructure:"BACKEND_HOSTNAME"`
+	HttpProto   string `mapstructure:"HTTP_PROTOCOL"`
+	Domain      string `mapstructure:"DOMAIN_NAME"`
+	BotId       uuid.UUID
+	BotIdString string `mapstructure:"BOT_ID"`
+	BotName     string `mapstructure:"CHAT_BOT_NAME"`
+	Assets      string `mapstructure:"ASSET_DIR"`
 }
 
 func (a *AppConf) SetBotId(id uuid.UUID) {
 	a.BotId = id
+	v.Set("BOT_ID", id)
+	err := v.WriteConfig()
+	if err != nil {
+		log.Fatal("Failed to write bot id to config.", err)
+	}
 }
 
 func LoadConfig() {
-	v := viper.New()
 	v.SetConfigName("app")
 	v.AddConfigPath("conf/")
 
@@ -43,5 +51,12 @@ func LoadConfig() {
 	fmt.Printf("%+v", App)
 	if err != nil {
 		panic(fmt.Errorf("fatal error unmarshalling config: %w", err))
+	}
+
+	if App.BotIdString != "" {
+		App.BotId, err = uuid.Parse(App.BotIdString)
+		if err != nil {
+			log.Fatalln("Failed to parse bot id.", err)
+		}
 	}
 }
