f158eaed93dcbec9d09885ae82be0986ff7ffb62
anytimesoon <damien@delartigue.co.uk>
2023-11-30 22:52:54 +0100

    make theme color persistent and improve light theme slightly (still needs work)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
     import "../app.css";
+
+    let theme = localStorage.getItem("theme") || "classic"
+
+    document.querySelector("html")?.setAttribute("data-theme", theme);
 </script>
 
 
