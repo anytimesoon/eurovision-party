8615d5940eafeb223c3d6087906411043fdded0d
anytimesoon <damien@delartigue.co.uk>
2024-01-29 13:32:09 +0100

    change button colour

--- frontend/src/lib/components/chat/ReplyMenu.svelte
+++ frontend/src/lib/components/chat/ReplyMenu.svelte
@@ -2,6 +2,8 @@
     export let replyButtonHandler:Function
 </script>
 
-<div on:mouseup={replyButtonHandler} class="absolute -top-4 right-2 bg-gray-600 px-2 rounded border border-secondary cursor-pointer">
+<div
+     on:mouseup={replyButtonHandler}
+     class="absolute -top-4 right-2 bg-chat-bubble-you px-2 rounded border border-secondary cursor-pointer">
     <i class="fa-solid fa-reply text-typography-main fa-xs"></i>
 </div>
