31b3d923daa00927d5178fd6b2e5bbc78c7fb95b
anytimesoon <damien@delartigue.co.uk>
2024-04-12 21:52:31 +0200

    transition and lazy loading images

---
+++ frontend/src/lib/components/images/ImageLoader.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+    import IntersectionObserver from "$lib/components/images/IntersectionObserver.svelte";
+    import Image from "$lib/components/images/Image.svelte";
+
+    export let src:string
+    export let alt:string
+    export let customClasses:string
+    let intersecting = false
+
+</script>
+
+<IntersectionObserver once={true} bind:intersecting={intersecting}>
+    {#if intersecting}
+        <Image {alt} {src} {customClasses}/>
+    {/if}
+</IntersectionObserver>
