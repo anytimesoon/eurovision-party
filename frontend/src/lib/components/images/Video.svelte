7bfd530e749446d2386cb324fcc30a6f71d4a535
anytimesoon <damien@delartigue.co.uk>
2024-05-10 23:03:37 +0200

    add web-m support

---
+++ frontend/src/lib/components/images/Video.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import { onMount } from 'svelte'
+    import {staticSvelteEP} from "$lib/models/enums/endpoints.enum";
+
+    export let src:string
+    export let alt:string
+    export let customClasses:string
+    let loaded = false
+    let thisImage:HTMLVideoElement
+
+    onMount(() => {
+        thisImage.onloadeddata = () => {
+            console.log('loaded')
+            loaded = true
+        }
+    })
+
+    $: imageOpacity = loaded ? "opacity-100" : "opacity-0"
+    $: pulse = loaded ? "" : "animate-pulse"
+</script>
+
+<div class="bg-canvas-secondary {pulse}">
+    {#if src.includes("mp4")}
+        <div class="bg-canvas-secondary">
+            <video autoplay loop muted playsinline bind:this={thisImage} class="{imageOpacity} {customClasses} transition-opacity ease-out duration-1000" >
+                <source src={src} type="video/mp4" />
+            </video>
+        </div>
+    {:else if src.includes("webm")}
+        <div class="bg-canvas-secondary">
+            <video autoplay loop muted playsinline bind:this={thisImage} class="{imageOpacity} {customClasses} transition-opacity ease-out duration-1000" >
+                <source src={src} type="video/webm" />
+            </video>
+        </div>
+    {/if}
+</div>
---
+++ frontend/src/lib/components/images/VideoLoader.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+
+    import IntersectionObserver from "$lib/components/images/IntersectionObserver.svelte";
+    import Image from "$lib/components/images/Image.svelte";
+    import Video from "$lib/components/images/Video.svelte";
+
+    export let src:string
+    export let alt:string
+    export let customClasses:string
+    let intersecting = false
+
+</script>
+
+<IntersectionObserver once={true} bind:intersecting={intersecting}>
+    {#if intersecting}
+        <Video {alt} {src} {customClasses}/>
+    {/if}
+</IntersectionObserver>
