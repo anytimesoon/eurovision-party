a6a9119147ec0469acb0622a6e782aa9c0ff1c83
anytimesoon <damien@delartigue.co.uk>
2024-05-16 13:36:34 +0200

    performance improvements for comment queue

--- frontend/src/lib/stores/comment.store.ts
+++ frontend/src/lib/stores/comment.store.ts
@@ -1,14 +1,26 @@
-import { writable } from "svelte/store";
+import {derived, writable} from "svelte/store";
 import type { CommentModel } from "$lib/models/classes/comment.model";
-import {browser} from "$app/environment";
-import {CountryModel} from "$lib/models/classes/country.model";
 
 const defaultCommentList:CommentModel[] = new Array<CommentModel>()
 
-// export const commentStore = writable<CommentModel[]>(browser && JSON.parse(localStorage.getItem("commentStore") || JSON.stringify(defaultCommentList)));
-//
-// commentStore.subscribe((val) => {
-//     browser && localStorage.setItem("commentStore", JSON.stringify(val))
-// });
-
 export const commentStore = writable<CommentModel[]>(defaultCommentList);
+
+const offset:number = 300
+
+export const recentComments = derived(commentStore, $commentStore => {
+    const currentLength = $commentStore.length
+    if (currentLength < offset) {
+        return $commentStore
+    } else {
+        return $commentStore.slice($commentStore.length - offset, $commentStore.length)
+    }
+})
+
+export const olderComments = derived(commentStore, $commentStore => {
+    const currentLength = $commentStore.length
+    if (currentLength < offset) {
+        return []
+    } else {
+        return $commentStore.slice(0, $commentStore.length - offset)
+    }
+})
